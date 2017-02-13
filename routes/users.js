8af5738a5a4a5ced59a815e4d9c52b87deeb427e
Max Yi-Hsun Chou <mrttra@Maxs-MacBook-Air.local>
2017-02-13 17:24:04 +0800

    - workable version

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,39 @@ var secretForToken = config.get('secretForToken');
 
 var User  = require('../models/users.js');
 
+//Add User
+router.post('/auth/create', function(req, res, next) {
+  var username = req.body.username
+  var password = req.body.password
+  var name     = req.body.name
+  var email    = req.body.email
+
+  var tokenGen = randomstring.generate();
+
+  var passwordHashed = crypto.createHmac('RSA-SHA512', secret)
+                             .update(password)
+                             .digest('hex');
+  var tokenHashed    = crypto.createHmac('RSA-SHA512', secretForToken)
+                             .update(tokenGen)
+                             .digest('hex');
+
+  var newUser ={
+   username: username,
+   password: passwordHashed,
+   name: name,
+   email: email,
+   token: tokenHashed
+  }
+
+  User.create(newUser, function(err, userCreate){
+    if(err) {
+      res.status(500).send({'message': 'Create User Failed, Username is repet'})
+    } else {
+      res.status(200).send({'message': 'Create User Success', 'token': tokenGen})
+    }
+  })
+});
+
 //Auth by Username and Password, Will get a token If you have success verifly
 router.post('/auth', function(req, res, next) {
   var username = req.body.username
@@ -20,8 +53,10 @@ router.post('/auth', function(req, res, next) {
                              .update(password)
                              .digest('hex');
   var tokenHashed    = crypto.createHmac('RSA-SHA512', secretForToken)
-                             .update(tokenHashed)
+                             .update(tokenGen)
                              .digest('hex');
+
+//rh80uzg7TjWolIVwDe2X0BKDyTaKnj79
 
   User.findOne({'username': username, 'password': passwordHashed}, function(err, userAuth){
       if (err) {
@@ -46,7 +81,7 @@ router.post('/auth', function(req, res, next) {
 });
 
 //Auth By Token
-router.get('/auth/token', function(req, res, next) {
+router.get('/auth/', function(req, res, next) {
   var token = req.query.token
 
   var tokenHashed = crypto.createHmac('RSA-SHA512', secretForToken)
